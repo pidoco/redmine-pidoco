5b761c37f363e77c560de5dd616886244b6384ae
yeah <yeah@08e4d922-58db-4713-a1e9-07da757211e0>
2010-03-16 19:29:18 +0000

    disable toolbar if pidoco is not enabled
    git-svn-id: https://rocket.plan.io/svn/redmine-pidoco/trunk@34 08e4d922-58db-4713-a1e9-07da757211e0

--- lib/pidoco_wiki_formatting_helper_patch.rb
+++ lib/pidoco_wiki_formatting_helper_patch.rb
@@ -8,17 +8,21 @@ module PidocoWikiFormattingHelperPatch
   end
   module InstanceMethods
     def wikitoolbar_for_with_pidoco_wikitoolbar(field_id)
-      help_link = l(:setting_text_formatting) + ': ' +
-        link_to(l(:label_help), compute_public_path('wiki_syntax', 'help', 'html'),
-                :onclick => "window.open(\"#{ compute_public_path('wiki_syntax', 'help', 'html') }\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
+      if @project and @project.module_enabled?(:pidoco)
+        help_link = l(:setting_text_formatting) + ': ' +
+          link_to(l(:label_help), compute_public_path('wiki_syntax', 'help', 'html'),
+                  :onclick => "window.open(\"#{ compute_public_path('wiki_syntax', 'help', 'html') }\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
 
-      stylesheet_link_tag("pidoco", :plugin => 'redmine_pidoco') +
-        javascript_include_tag('jstoolbar/jstoolbar') +
-        javascript_include_tag('jstoolbar/textile') +
-        render(:partial => 'wiki/pidocobar') +
-        #javascript_include_tag("pidocobar", :plugin => 'redmine_pidoco') +
-        javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language}") +
-        javascript_tag("var toolbar = new jsToolBar($('#{field_id}')); toolbar.setHelpLink('#{help_link}'); toolbar.draw();")
+        stylesheet_link_tag("pidoco", :plugin => 'redmine_pidoco') +
+          javascript_include_tag('jstoolbar/jstoolbar') +
+          javascript_include_tag('jstoolbar/textile') +
+          render(:partial => 'wiki/pidocobar') +
+          #javascript_include_tag("pidocobar", :plugin => 'redmine_pidoco') +
+          javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language}") +
+          javascript_tag("var toolbar = new jsToolBar($('#{field_id}')); toolbar.setHelpLink('#{help_link}'); toolbar.draw();")
+      else
+        wikitoolbar_for_without_pidoco_wikitoolbar(field_id)
+      end
     end
   end
 end
