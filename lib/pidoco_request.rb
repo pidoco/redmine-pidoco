adf17c3922000a31308faa088bdc37a9bc7ca2f4
martin.kreichgauer <martin.kreichgauer@08e4d922-58db-4713-a1e9-07da757211e0>
2010-03-22 17:57:09 +0000

    - optimized view to minimize possible api requests - reduced time out value for subsequent api requests
    git-svn-id: https://rocket.plan.io/svn/redmine-pidoco/trunk@58 08e4d922-58db-4713-a1e9-07da757211e0

--- lib/pidoco_request.rb
+++ lib/pidoco_request.rb
@@ -28,7 +28,7 @@ module PidocoRequest
     date = Setting[:plugin_redmine_pidoco]["date_" + request_uri]
     # Don't request more than once every 60 seconds. Otherwise we would end up requesting the prototype too often
     # when displaying all discussions, e.g.
-    if date.try(:length) && ((Time.parse(date) + 60) > Time.now) && caching
+    if date.try(:length) && ((Time.parse(date) + 20) > Time.now) && caching
       log_message = "skipping request for " + uri + ", too frequent"
       RAILS_DEFAULT_LOGGER.info(log_message)
       return nil
