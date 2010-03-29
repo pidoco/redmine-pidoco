91b52c37ba7d0b44227fbe14071b5c474087ffe5
martin.kreichgauer <martin.kreichgauer@08e4d922-58db-4713-a1e9-07da757211e0>
2010-03-29 16:30:42 +0000

    fixed the horribly broken migration
    git-svn-id: https://rocket.plan.io/svn/redmine-pidoco/trunk@63 08e4d922-58db-4713-a1e9-07da757211e0

--- db/migrate/007_add_pidoco_key_prototype_id.rb
+++ db/migrate/007_add_pidoco_key_prototype_id.rb
@@ -1,11 +1,15 @@
 class AddPidocoKeyPrototypeId < ActiveRecord::Migration
   def self.up
     add_column :pidoco_keys, :prototype_id, :integer
+    Setting[:plugin_redmine_pidoco] = {}
     Prototype.all.each do |prototype|
-      if prototype.pidoco_key
-        prototype.pidoco_key.prototype_id = prototype.id
-      else
-        prototype.destroy
+      key = PidocoKey.find_by_id(prototype.pidoco_key_id)
+      key.prototype_id = prototype.id
+      key.fetch_prototype # Refresh the data, just to be sure.
+    end
+    PidocoKey.all.each do |key|
+      unless key.prototype_id
+        key.destroy
       end
     end
   end
