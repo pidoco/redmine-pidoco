875339717cc0d13717b6606d9c9170d330fc0390
martin.kreichgauer <martin.kreichgauer@08e4d922-58db-4713-a1e9-07da757211e0>
2010-03-29 15:24:33 +0000

    added error handling to migration 7
    All prototypes without pidoco keys become inaccessible with this migration and should not be kept in the database.
    
    
    git-svn-id: https://rocket.plan.io/svn/redmine-pidoco/trunk@61 08e4d922-58db-4713-a1e9-07da757211e0

--- db/migrate/007_add_pidoco_key_prototype_id.rb
+++ db/migrate/007_add_pidoco_key_prototype_id.rb
@@ -2,7 +2,11 @@ class AddPidocoKeyPrototypeId < ActiveRecord::Migration
   def self.up
     add_column :pidoco_keys, :prototype_id, :integer
     Prototype.all.each do |prototype|
-      prototype.pidoco_key.prototype_id = prototype.id
+      if prototype.pidoco_key
+        prototype.pidoco_key.prototype_id = prototype.id
+      else
+        prototype.delete
+      end
     end
   end
   
