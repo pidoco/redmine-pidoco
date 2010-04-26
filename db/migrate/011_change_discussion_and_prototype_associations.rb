f21b4619592bf60fb9a3d0056e451f01b2370e1d
martin.kreichgauer <martin.kreichgauer@08e4d922-58db-4713-a1e9-07da757211e0>
2010-04-26 08:31:45 +0000

    added forgotten migration
    git-svn-id: https://rocket.plan.io/svn/redmine-pidoco/trunk@71 08e4d922-58db-4713-a1e9-07da757211e0

---
+++ db/migrate/011_change_discussion_and_prototype_associations.rb
@@ -0,0 +1,19 @@
+class ChangeDiscussionAndPrototypeAssociations < ActiveRecord::Migration
+  def self.up
+    # Don't use pidoco's internal ids as AR ids
+    add_column :prototypes, :api_id, :integer
+    add_column :discussions, :api_id, :integer
+    Prototype.all.each do |prototype|
+      prototype.api_id = prototype.id
+      prototype.save!
+    end
+    Discussion.all.each do |discussion|
+      discussion.api_id = discussion.id
+      discussion.save!
+    end
+  end
+  
+  def self.down
+
+  end
+end
